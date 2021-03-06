3a233e8c17080a389edae3c9a1279937395f1966
Hristo <hristo4all@abv.bg>
2021-03-06 16:51:05 +0200

    added nedb

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const nedb = require("nedb");
+
+const db = new nedb({ filename: "student.db", autoload: true });
+console.log("db created");
+
+db.insert(
+  {
+    student: "Peter",
+    grant: false,
+    modules: [
+      {
+        name: "Programming",
+        grade: 70,
+      },
+      {
+        name: "Databases",
+        grade: 70,
+      },
+      {
+        name: "Networking",
+        grade: 80,
+      },
+    ],
+  },
+  function (err, newDoc) {
+    if (err) {
+      console.log("error", err);
+    } else {
+      console.log("document inserted: ", newDoc);
+    }
+  }
+);
+db.find({ student: "Peter" }, function (err, docs) {
+  if (err) {
+    console.log("error");
+  } else {
+    console.log("documents retrieved:", docs);
+  }
+});
+
+db.update(
+  { student: "Peter", "modules.name": "Networking" },
+  { $set: { "modules.grade": 80 } },
+  {},
+  function (err, numUp) {
+    if (err) {
+      console.log("error updating documents", err);
+    } else {
+      console.log(numUp, "document(s) updated");
+    }
+  }
+);
+/*
+db.remove({ student: "Peter" }, {}, function (err, docsRem) {
+  if (err) {
+    console.log("error deleting document Peter");
+  } else {
+    console.log(docsRem, "document(s) removed from database");
+  }
+});
 
 const public = path.join(__dirname, "public");
 app.use(express.static(public));
